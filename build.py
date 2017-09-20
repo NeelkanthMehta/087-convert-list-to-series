ffb8ccc453e5ce6a35407b2db504bcc3f15052b5
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-09-20 13:34:50 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
-import pandas as pd
-
-
 def solution(array):
     """
     Enter your code here
     """
+    from pandas import  Series
+    return Series(data = array)
+
+alist = [2,4,6,8,10]
+solution(alist)
